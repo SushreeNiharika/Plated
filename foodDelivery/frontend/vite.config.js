4997c8cc262022685a2277bac7d7acc632db9180
SushreeNiharika <sushree.niharika666@gmail.com>
2024-12-17 23:10:09 +0530

    vite js changes

--- foodDelivery/frontend/vite.config.js
+++ foodDelivery/frontend/vite.config.js
@@ -1,10 +1,7 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/", // For root deployment, use '/' (if it's deployed at a subpath, replace with '/myapp/')
   plugins: [react()],
-  build: {
-    outDir: "dist", // This will make Vite output the build files to the dist directory
-  },
 });
