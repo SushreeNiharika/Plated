190ee07b988ec02578bae8798d915e2974643278
SushreeNiharika <sushree.niharika666@gmail.com>
2024-12-18 19:30:50 +0530

    backend url added

--- foodDelivery/admin/src/pages/Add/Add.jsx
+++ foodDelivery/admin/src/pages/Add/Add.jsx
@@ -5,7 +5,8 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 const Add = () => {
-  const url = "http://localhost:4000";
+  //const url = "http://localhost:4000";
+   = "https://platted-backend-up.onrender.com";
   const [image, setImage] = useState(false);
   const [data, setData] = useState({
     name: "",
--- foodDelivery/frontend/src/context/StoreContext.jsx
+++ foodDelivery/frontend/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setcartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = "https://platted-backend-up.onrender.com";
   const [token, setToken] = useState("");
 
   const [food_list, setFood_list] = useState([]);
