c6d13a9e5a39099534de5b78eb0be500f3514719
SushreeNiharika <sushree.niharika666@gmail.com>
2024-12-18 20:09:07 +0530

    backend url

--- foodDelivery/admin/src/pages/Add/Add.jsx
+++ foodDelivery/admin/src/pages/Add/Add.jsx
@@ -6,7 +6,7 @@ import { toast } from "react-toastify";
 
 const Add = () => {
   //const url = "http://localhost:4000";
-   = "https://platted-backend-up.onrender.com";
+  const url = "https://platted-backend-up.onrender.com";
   const [image, setImage] = useState(false);
   const [data, setData] = useState({
     name: "",
