a8c1064da59034b5a37ceeb8540251a8786e8c0a
Sushree Niharika Purohit <167462412+SushreeNiharika@users.noreply.github.com>
2024-12-19 15:14:24 +0530

    Update orderController.js verify to payment

--- foodDelivery/backend/controllers/orderController.js
+++ foodDelivery/backend/controllers/orderController.js
@@ -39,12 +39,19 @@ const placeOrder = async (req, res) => {
       quantity: 1,
     });
 
-    const session = await stripe.checkout.sessions.create({
+   /* const session = await stripe.checkout.sessions.create({
       line_items: line_items,
       mode: "payment",
       success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
       cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
-    });
+    });*/
+  const session = await stripe.checkout.sessions.create({
+  line_items: line_items,
+  mode: "payment",
+  success_url: `${frontend_url}/payment-status?success=true&orderId=${newOrder._id}`, // Redirect to /payment-status
+  cancel_url: `${frontend_url}/payment-status?success=false&orderId=${newOrder._id}`, // Redirect to /payment-status
+});
+
     res.json({
       success: true,
       session_url: session.url,
