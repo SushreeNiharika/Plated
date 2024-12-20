95816ad6111706cd2df656c6b4be5d938f4df0c3
SushreeNiharika <sushree.niharika666@gmail.com>
2024-12-20 14:03:06 +0530

    change

--- foodDelivery/admin/src/pages/List/List.jsx
+++ foodDelivery/admin/src/pages/List/List.jsx
@@ -31,7 +31,7 @@ const List = () => {
     const response = await axios.post(`${url}/api/food/remove`, { id: foodId });
     await fetchList();
     if (response.data.success) {
-      toast.success(response.data.message);
+      toast.success(response.data.message || "Food item removed successfully.");
     } else {
       toast.error("Failed to remove");
     }
